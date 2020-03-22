a77f655be5b6dfe6cd6e10506292812a8a0d7bf6
Mojtaba <mojtaba.abbasszadeh@gmail.com>
2020-03-21 22:32:04 -0400

    view model removed, create button added, finlized

--- app/src/main/java/com/example/prep_authentication_integration/MainActivity.java
+++ app/src/main/java/com/example/prep_authentication_integration/MainActivity.java
@@ -4,62 +4,115 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.prep_authentication_integration.database.UserDAO;
+import com.example.prep_authentication_integration.database.UserDatabase;
 import com.example.prep_authentication_integration.database.UserEntity;
 import com.example.prep_authentication_integration.utilities.SampleUsers;
 
 import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<UserEntity> mUsers = new ArrayList<>();
+    EditText usernameInput;
+    EditText passwordInput;
+    TextView textView;
+    private UserDatabase mDb;
+    private Executor executor = Executors.newSingleThreadExecutor();
+    final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        usernameInput = findViewById(R.id.user_name_text);
+        passwordInput = findViewById(R.id.password_Text);
+        textView = findViewById(R.id.textView);
+
         Button logInButton = findViewById(R.id.log_in);
         logInButton.setOnClickListener(v -> loginButtonClicked());
 
-        addUsers();
-        showUsers();
+        Button createAccountButton = findViewById(R.id.creat_account);
+        createAccountButton.setOnClickListener(v -> createAccountButtonClicked());
+
+        addSampleUsers();
+//        showUsers();
+    }
+
+    private void createAccountButtonClicked() {
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        UserEntity user = new UserEntity(username,password);
+        Log.i(TAG, "checking information for username: " + username + ", and password: " + password);
+
+        executor.execute((() -> {
+            mDb.userDAO().insertUser(user);
+            runOnUiThread(() -> {
+                Toast.makeText(this, "New user created", Toast.LENGTH_LONG).show();
+                usernameInput.setText("");
+                passwordInput.setText("");
+            });
+
+        }));
 
     }
 
-    private void addUsers() {
-        mUsers.addAll(SampleUsers.getUsers());
-        UserDAO mUsersData;
+    /**
+     * Add a few sample users for testing purposes.
+     */
+    private void addSampleUsers() {
+        executor.execute(() -> {
+            mDb = UserDatabase.getInstance(getApplicationContext());
+            mDb.userDAO().insertAll(SampleUsers.getUsers());
+        });
     }
 
     private void showUsers() {
-        TextView textView = findViewById(R.id.textView);
-
-        textView.setText(mUsers.toString());
+        executor.execute(() -> {
+            ArrayList<UserEntity> users = (ArrayList<UserEntity>) mDb.userDAO().getAllUsers();
+            runOnUiThread(() -> textView.setText(users.toString()));
+        });
     }
 
     public void loginButtonClicked() {
 
-        if (checkPasswordValidity()) {
-            Intent intent = new Intent(this, MapsActivity.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(MainActivity.this, "Wrong password!",
-                    Toast.LENGTH_LONG).show();
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        Log.i(TAG, "checking information for username: " + username + ", and password: " + password);
+
+        executor.execute((() -> {
+            UserEntity user = mDb.userDAO().getUserByUserName(username);
+            runOnUiThread(() -> validateUser(user));
+        }));
+    }
+
+    public void validateUser(UserEntity user) {
+
+        if (user == null) {
+            Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show();
+            return;
         }
+        Log.i(TAG, "user found, password should be: " + user.getPassword());
+        String password = passwordInput.getText().toString();
+        if (!password.equals(user.getPassword())) {
+            Toast.makeText(MainActivity.this, "Password is incorrect", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 
     private boolean checkPasswordValidity() {
-
-//        اینو کجا باید تعریف کنم؟ اگر تو آنکریت تعریف کنم، چطوری اینجا استفاده کنم؟
-        EditText usernameInput = findViewById(R.id.user_name_text);
 
         if (usernameInput.getText().toString().equals("a")) {
             return true;
